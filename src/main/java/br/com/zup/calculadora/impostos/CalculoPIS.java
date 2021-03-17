397865b6e358f4d35d5a468dad3c46d5f533d76b
Iris <iris.souza@zup.com.br>
2021-03-17 12:54:54 -0300

    Adicionar classe CalculoPIS

---
+++ src/main/java/br/com/zup/calculadora/impostos/CalculoPIS.java
@@ -0,0 +1,13 @@
+package br.com.zup.calculadora.impostos;
+
+public class CalculoPIS implements Tributavel {
+
+    private double valorProduto;
+    private double imposto = 0.0065;
+
+    @Override
+    public double calcularImposto() {
+        return valorProduto * imposto;
+    }
+
+}

39776f99efa9deb65a7abe4a3e42d69fabc7908c
Iris <iris.souza@zup.com.br>
2021-03-17 11:42:38 -0300

    Criar projeto

---
+++ src/main/java/br/com/zup/calculadoraimpostos/CalculadoraImpostosApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.calculadoraimpostos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculadoraImpostosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculadoraImpostosApplication.class, args);
+	}
+
+}
